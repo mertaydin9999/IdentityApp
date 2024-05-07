f94d49900dc72076aeb2f4519aca6d3a2c4593e6
mertaydin9999 <mertaydin9999@hotmail.com>
2024-05-07 17:19:52 +0300

    hesap onay linki

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -22,6 +22,7 @@ namespace IdentityApp.Controllers
         }
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
+
         {   
             if(ModelState.IsValid)
             {
@@ -30,6 +31,13 @@ namespace IdentityApp.Controllers
                 if(user !=null)
                 {
                     await _signInManager.SignOutAsync();
+
+                    if(!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError("","Hesabinizi onaylayiniz.");
+                        return View(model);
+                    }
+
                     var result = await _signInManager.PasswordSignInAsync(user,model.Password,model.RememberMe,true);
 
                     if(result.Succeeded)
@@ -57,6 +65,64 @@ namespace IdentityApp.Controllers
                 }
             }
             return View(model);
+        }
+         public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new AppUser{
+                    UserName =model.UserName,
+                    Email = model.Email,
+                    FullName = model.FullName
+                };
+                IdentityResult result = await _userManager.CreateAsync(user,model.Password);
+                if(result.Succeeded)
+                {
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var url = Url.Action("ConfirmEmail","Account",new { user.Id,token });
+
+                    //email
+                    TempData["message"] ="Email hesabinizdaki onay mailine tiklayiniz";
+                    return RedirectToAction("Login","Account");
+                }
+                foreach(IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("",err.Description);
+                }
+                
+            }
+
+            return View(model);
+        }
+        
+        public async Task<IActionResult> ConfirmEmail(string Id,string token)
+        {
+            if(Id == null || token == null)
+            {
+                TempData["message"] ="Gecersiz token bilgisi";
+                return View();
+            }
+
+            var user = await _userManager.FindByIdAsync(Id);
+            if(user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user,token);
+                if(result.Succeeded)
+                {
+                    TempData["message"] ="Hesabiniz Onaylandi";
+                    return RedirectToAction("Login","Account");
+                }
+
+            }
+
+            TempData["message"] ="Kullanici Bulunamadi";
+            return View();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<IdentityContext>(
     options => options.UseSqlite(builder.Configuration["ConnectionStrings:SQLite_Connection"]));
-builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<IdentityContext>();
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
 
 builder.Services.Configure<IdentityOptions>(options => {
     options.Password.RequiredLength = 6;
@@ -20,6 +20,9 @@ builder.Services.Configure<IdentityOptions>(options => {
 
     options.Lockout.DefaultLockoutTimeSpan =TimeSpan.FromMinutes(5);
     options.Lockout.MaxFailedAccessAttempts = 5;
+
+    options.SignIn.RequireConfirmedEmail = true;
+
     
 });
 builder.Services.ConfigureApplicationCookie(options => {
